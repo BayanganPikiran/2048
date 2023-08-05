780cd59d74a96bd4e2cd12fbbfed3d46f7d81c25
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-08-06 06:44:32 +0700

    self.game_frame and self.footer_frame created

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from constants import *
 import numpy as np
 
+
 class Board:
 
     def __init__(self):
@@ -11,6 +12,21 @@ class Board:
         self.root.resizable(None, None)
         self.root.configure(background=PLAYFIELD_GRAY)
         self.root.title("2048")
+
+        self.game_frame = self.create_game_frame()
+        self.footer_frame = self.create_footer_frame()
+
+    # ----------------------------- Frames ------------------------------- #
+    def create_game_frame(self):
+        game_frame = tk.Frame(self.root, width=PLAY_WIDTH, height=PLAY_HEIGHT, bg=PLAYFIELD_GRAY)
+        game_frame.pack(anchor=tk.CENTER, expand=True)
+        return game_frame
+
+    def create_footer_frame(self):
+        footer = tk.Frame(self.root, width=FOOTER_WIDTH, height=FOOTER_HEIGHT, bg=FOOTER_GRAY)
+        footer.pack(anchor=tk.CENTER, expand=True)
+        return footer
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
