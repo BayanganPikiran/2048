562248368cd23fb797becbaf336d487084d9e9e9
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-08-12 08:45:54 +0700

    changed compress_matrix().  It's so much better now

--- main.py
+++ main.py
@@ -2,16 +2,8 @@ import random
 import tkinter as tk
 from constants import *
 import numpy as np
+from tkinter import messagebox
 
-sample_matrix = [[2, 2, 4, 8], [8, 8, 16, 16], [4, 0, 4, 4], [32, 32, 64, 64]]
-
-sample_matrix_2 = [[2, 0, 2, 0], [0, 8, 0, 8], [0, 0, 16, 16], [32, 0, 0, 32]]
-
-sample_matrix_3 = [[2, 2, 4, 8], [16, 0, 16, 32], [32, 32, 0, 64], [64, 0, 0, 64]]
-
-sample_matrix_4 = [[0, 0, 0, 5], [0, 3, 0, 8], [0, 0, 2, 0], [1, 2, 0, 4]]
-
-score = 0
 
 
 class Board:
@@ -69,20 +61,19 @@ class Board:
         return hi_score_label
 
     def create_restart_btn(self):
-        restart_btn = tk.Button(self.footer_frame, text="Restart", width=5, height=1, font=LABEL_FONT)
+        restart_btn = tk.Button(self.footer_frame, text="Restart", width=5, height=1, font=LABEL_FONT,
+                                command=lambda: print("Big fat cock"))
         restart_btn.grid(row=0, column=1, rowspan=2, sticky=tk.E, padx=3, pady=2, ipadx=6, ipady=12)
         return restart_btn
 
     # ------------------------ Matrix Values & Cells ------------------------- #
     def create_board_matrix(self):
         matrix = [[0 for j in range(4)] for i in range(4)]
-        print(f"The starting matrix is: {matrix}")
         return matrix
 
     def choose_random_index(self):
         index_list = [0, 1, 2, 3]
         random_index = random.sample(index_list, 2)
-        print(f"choose_rand_index result: {random_index}")
         return random_index
 
     def draw_to_board(self, row, col, value):
@@ -92,7 +83,6 @@ class Board:
         self.board_squares[row][col].config(bg=BACKGROUND_COLORS.get(str(value)), fg=FONT_COLORS.get(str(value)))
 
     def populate_vacant_square(self):
-        # replaces matrix 0 with 2 or 4 and draws value to corresponding board location
         rand_ind = self.choose_random_index()
         rand_val = random.choice([2, 4])
         if self.board_matrix[rand_ind[0]][rand_ind[1]] == 0:
@@ -100,7 +90,6 @@ class Board:
             self.draw_to_board(rand_ind[0], rand_ind[1], str(rand_val))
         else:
             self.populate_vacant_square()
-        print(f"The new matrix values are {self.board_matrix}")
 
     def start_with_two(self):
         for i in range(2):
@@ -119,41 +108,26 @@ class Board:
 
     # ------------------------ Movement Configuration ------------------------- #
 
-    def squeeze_matrix(self):
-        for i in range(4):
-            for j in range(3):
-                while self.board_matrix[i][j] == 0 and self.board_matrix[i][j + 1] != 0:
-                    for row in range(4):
-                        for col in range(3):
-                            if self.board_matrix[row][col] == 0:
-                                self.board_matrix[row][col] = self.board_matrix[row][col + 1]
-                                self.board_matrix[row][col + 1] = 0
-        # print(f"This is our new matrix: {mx}")
 
     def compress_matrix(self):
-        self.squeeze_matrix()
+        temp_matrix = [[0] * 4 for x in range(4)]
         for i in range(4):
-            for j in range(3):
-                while self.board_matrix[i][j] == 0 and self.board_matrix[i][j + 1] != 0:
-                    if self.board_matrix[i][j] == 0:
-                        self.board_matrix[i][j] = self.board_matrix[i][j + 1]
-                        self.board_matrix[i][j + 1] = 0
-        print(f"The compressed matrix is: {self.board_matrix}")
+            temp_pos = 0
+            for j in range(4):
+                if self.board_matrix[i][j] != 0:
+                    temp_matrix[i][temp_pos] = self.board_matrix[i][j]
+                    temp_pos += 1
+        self.board_matrix = temp_matrix
 
     def transpose_matrix(self):
         tp_matrix = np.transpose(self.board_matrix).tolist()
         self.board_matrix = np.transpose(self.board_matrix).tolist()
-        print(f"This is the matrix: {self.board_matrix}")
-        print(f"This is the transposed matrix: {tp_matrix}")
 
     def reverse_matrix(self):
         r_matrix = np.flip(self.board_matrix).tolist()
         self.board_matrix = np.flip(self.board_matrix).tolist()
-        print(f"This is the matrix: {self.board_matrix}")
-        print(f"This is the reversed matrix: {r_matrix}")
 
     def merge_cells(self):
-        print(f"This is the starting matrix: {self.board_matrix}")
         for i in range(4):
             for j in range(3):
                 if self.board_matrix[i][j] == self.board_matrix[i][j + 1]:
@@ -161,8 +135,6 @@ class Board:
                     self.board_matrix[i][j + 1] = 0
                     self.score += self.board_matrix[i][j]
         self.score_label.config(text=self.score)
-        print(f"This is the new matrix: {self.board_matrix}")
-        print(f"This is the new score: {self.score}")
 
     def can_merge(self):
         for i in range(4):
@@ -171,13 +143,14 @@ class Board:
                     return True
         for i in range(3):
             for j in range(4):
-                if self.board_matrix[i][j]  == self.board_matrix[i + 1][j]:
+                if self.board_matrix[i][j] == self.board_matrix[i + 1][j]:
                     return True
         return False
 
     def link_keys(self, event):
         pressed_key = event.keysym
         if pressed_key == 'Up':
+            # problem: doesn't go from bottom to top
             self.transpose_matrix()
             self.compress_matrix()
             self.merge_cells()
@@ -199,6 +172,7 @@ class Board:
             self.populate_vacant_square()
             self.update_board_squares()
         if pressed_key == 'Right':
+            # problem: doesn't go from far left to far right
             self.reverse_matrix()
             self.compress_matrix()
             self.merge_cells()
@@ -206,13 +180,21 @@ class Board:
             self.populate_vacant_square()
             self.update_board_squares()
 
+    def play_game(self):
+        self.choose_random_index()
+        self.start_with_two()
+        self.root.mainloop()
+
+
+
+
+
+# def restart(board):
+#     board.root.destroy()
+#     board.play_game()
+
 
 if __name__ == '__main__':
     board = Board()
-    board.choose_random_index()
-    board.start_with_two()
+    board.play_game()
 
-    # board.take_a_turn(sample_matrix_3)
-    board.root.mainloop()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
